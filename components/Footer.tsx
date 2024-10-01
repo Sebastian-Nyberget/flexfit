92fa452c216e774fce93c13453e43f10ff55aec2
Sebastian-Nyberget <nybesebe@gmail.com>
2024-10-01 14:16:17 +0200

    update Membership.tsx + Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <Link href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <GiWeightLiftingUp className="text-black text-4xl" />
+                        <GiWeightLiftingUp className="text-black dark:text-white text-4xl" />
                         <span className={`${righteous.className} self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white`}>
                             FlexFit
                         </span>
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023 <Link href="/" className="hover:underline">FlexFit</Link>. All Rights Reserved.
+                    © 2024 <Link href="/" className="hover:underline">FlexFit</Link>. All Rights Reserved.
                 </span>
             </div>
         </footer>
--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,53 +1,135 @@
-import Image from 'next/image';
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 const Page = () => {
   return (
-    <div className="bg-white w-full h-full py-16">
+    <div className="bg-white dark:bg-slate-900 w-full h-full py-16">
       <div className="w-full flex flex-col items-center justify-center">
         <p className='text-primary'>Priser</p>
-        <h1 className="text-black text-4xl font-bold">Medlemskap</h1>
+        <h1 className="text-black dark:text-white text-4xl font-bold">Medlemskap</h1>
         <div className="pt-4">
-          <div className="bg-slate-900 w-96 h-auto flex rounded-xl py-4 px-2">
+          <div className="bg-black dark:bg-white w-96 h-auto flex rounded-xl py-3 px-2">
             <div className='w-1/2 text-center bg-primary rounded-xl py-2'>Månedlig Fakturering</div>
-            <div className='w-1/2 text-center py-2 opacity-60'>Årlig Fakturering</div>
+            <div className='w-1/2 text-center py-2 opacity-60 text-white dark:text-black'>Årlig Fakturering</div>
           </div>
         </div>
       </div>
 
       <div className="flex flex-wrap justify-center gap-8 px-20 pt-10">
-        <div className="max-w-xs bg-slate-900 rounded-xl shadow-lg overflow-hidden">
+        <div className="max-w-xs bg-black rounded-xl shadow-lg overflow-hidden">
           <div className="w-full h-80 px-4 py-6">
             <h1 className="text-left text-primary font-semibold text-xl">Basic Package</h1>
             <h1 className='text-4xl font-semibold'>980kr</h1>
-            <p className="text-left text-gray-300 mt-2">This is the perfect package for beginners looking to get started.</p>
-            <p className="text-left text-white mt-2 font-bold">$19.99/month</p>
+            <p className="text-left text-gray-300 py-2">per måned</p>
+            <ul>
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til treningsenter</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til gruppetimer</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til garderober</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Én gratis treningsvurdering per år</p>
+              </li>
+            </ul>
           </div>
         </div>
 
-        <div className="max-w-xs bg-primary rounded-xl shadow-lg overflow-hidden">
-          <div className="w-full h-80 px-4 py-6">
-            <h1 className="text-left text-white font-semibold text-xl">Mid Package</h1>
-            <h1 className='text-4xl font-semibold'>980kr</h1>
-            <p className="text-left text-gray-300 mt-2">This is the perfect package for beginners looking to get started.</p>
-            <p className="text-left text-white mt-2 font-bold">$19.99/month</p>
+        <div className="max-w-xs bg-primary rounded-xl shadow-lg overflow-hidden relative">
+          <div className="bg-white absolute text-black font-semibold rotate-45 right-2 top-10 w-32 flex justify-center">Best Offer</div>
+        <div className="w-full h-80 px-4 py-6">  
+          <h1 className="text-left font-semibold text-xl">Mid Package</h1>
+          <h1 className='text-4xl font-semibold'>980kr</h1>
+          <p className="text-left text-gray-300 py-2">per måned</p>
+            <ul>
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til treningsenter</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til gruppetimer</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til garderober</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Én gratis treningsvurdering per år</p>
+              </li>
+            </ul>
           </div>
         </div>
 
-        <div className="max-w-xs bg-slate-900 rounded-xl shadow-lg overflow-hidden">
+        <div className="max-w-xs bg-black rounded-xl shadow-lg overflow-hidden">
           <div className="w-full h-80 px-4 py-6">
             <h1 className="text-left text-primary font-semibold text-xl">Pro Package</h1>
             <h1 className='text-4xl font-semibold'>980kr</h1>
-            <p className="text-left text-gray-300 mt-2">This is the perfect package for beginners looking to get started.</p>
-            <p className="text-left text-white mt-2 font-bold">$19.99/month</p>
+            <p className="text-left text-gray-300 py-2">per måned</p>
+            <ul>
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til treningsenter</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <span><IoIosCheckmarkCircle /> </span>
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til gruppetimer</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til garderober</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Én gratis treningsvurdering per år</p>
+              </li>
+            </ul>
           </div>
         </div>
 
-        <div className="max-w-xs bg-slate-900 rounded-xl shadow-lg overflow-hidden">
+        <div className="max-w-xs bg-black rounded-xl shadow-lg overflow-hidden">
           <div className="w-full h-80 px-4 py-6">
             <h1 className="text-left text-primary font-semibold text-xl">Athlete Package</h1>
             <h1 className='text-4xl font-semibold'>980kr</h1>
-            <p className="text-left text-gray-300 mt-2">This is the perfect package for beginners looking to get started.</p>
-            <p className="text-left text-white mt-2 font-bold">$19.99/month</p>
+            <p className="text-left text-gray-300 py-2">per måned</p>
+            <ul>
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til treningsenter</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til gruppetimer</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Tilgang til garderober</p>
+              </li>
+
+              <li className="flex text-lg py-1 items-center">
+                <IoIosCheckmarkCircle />
+                <p className="text-white font-bold ml-2">Én gratis treningsvurdering per år</p>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
