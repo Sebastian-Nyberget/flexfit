82907e013979569480116e7657c11ab009ec5764
Sebastian-Nyberget <nybesebe@gmail.com>
2024-10-25 13:37:24 +0200

    googlemap update

--- features/contact/components/googlemap.tsx
+++ features/contact/components/googlemap.tsx
@@ -13,18 +13,8 @@ const center = {
 };
 
 const GoogleMapComponent = () => {
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-
-  if (!apiKey) {
-    return <div>Error: Google Maps API key is missing.</div>;
-  }
-
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <iframe className='w-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1946.0748479231324!2d11.051716877417729!3d60.80652359191502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4641e11ed2aa2b19%3A0x7d0684a2db521496!2sRinggata%20235%2C%202315%20Hamar!5e0!3m2!1sno!2sno!4v1729503362175!5m2!1sno!2sno" loading="lazy"></iframe>
   );
 };
 
